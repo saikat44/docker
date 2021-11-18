b2d6e1588c376519d30df3d0c4dc63f684852892
Saikat Mukherjee <mukherjeesaikat@in.ibm.com>
2021-11-18 15:20:58 +0530

    update

--- portainer/portainer-win.sh
+++ portainer/portainer-win.sh
@@ -1,2 +1,2 @@
+docker volume create portainer_data
 docker run -d -p 9000:9000 --name portainer --restart=always -v portainer_data:/data portainer/portainer --no-auth -H tcp://docker.for.win.localhost:2375 &
-
